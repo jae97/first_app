acfbfb756874ecc188ccdbb0e6f191ddcc18a63b
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-17 13:43:30 -0700

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 st.title('My first app')
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df

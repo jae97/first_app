55a8d7ff898c54c9c20bb1124935115d7c61d8ee
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-17 13:48:44 -0700

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -18,7 +18,7 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [10, 10] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
